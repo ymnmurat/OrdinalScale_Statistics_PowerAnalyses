c909c5bdf7d1c09e6a9872161227496b419e4bc2
ymnmurat <88300302+ymnmurat@users.noreply.github.com>
2023-02-26 11:50:20 -0500

    Update Test_OrdinalScaleStatistics_PowerAnalyses.R

--- Test_OrdinalScaleStatistics_PowerAnalyses.R
+++ Test_OrdinalScaleStatistics_PowerAnalyses.R
@@ -41,16 +41,16 @@ dat=ordinalScaleStats(dataset = dat, padjval = T)
 
 dataset_long= dat %>% group_by(Exposures, Scores, Counts) |> uncount(Counts) %>% dplyr::select(Exposures, Scores) %>% as.data.frame()
 
-#Let's estimate the power levels and obtain representative figures. Note1: Since there some computational power needed to generate simulations, you may need to wait for a while for the function to execute. Note2: you may be prompted to modify some of the arguments, in case there is some misspelling or missing data. For example, the command below will prompt you with correcting your control, and will take EXP2 and exp3 into account in making the analyses, since there is no group called Experiment1 in our samples, and the function can handle case-sensitivity.
+#Let's estimate the power levels and obtain representative figures. Note1: Since there is some computational power needed to generate simulations, you may need to wait for a while for the function to execute. Note2: you may be prompted to modify some of the arguments, in case there is some misspelling or missing data. For example, the command below will prompt you with correcting your control, and will take EXP2 and exp3 into account in making the analyses, since there is no group called Experiment1 in our samples, yet the function can handle case-sensitivity.
 a=powerEstimator(dataset = dataset_long,refgroup = 'controllls', powergroups = c('Experiment1', 'EXP2','exp3'))
 
 #No need to provide a powergroups argument if you want to test for all groups in your experiment. It will automatically assign the groups for you/ Please, just be careful with the reference argument. If not provided, the function will automatically select the first group from the list of groups in your data.
 b=powerEstimator(dataset = dataset_long,refgroup = 'controls')
 
-#You can also change the power level (default 80), range of sample sizes  (default 10-250) to be tested and the set.seed (default 1994) arguments
+#You can also change the power level (default 80), range of sample sizes (default 10-250) to be tested and the set.seed (default 1994) arguments
 c=powerEstimator(dataset = dataset_long,refgroup = 'controls', powerlevel = 90, seedlevel = 2, nsample_min = 30, nsample_max = 500, nsample_step = 50)
 
-#If you prefer, you can have a look at the total experiment size (N) needed for the whole experiment, instead of group sizes  (n). In addition, you can also generate static plots rather than interactive plots and html documents
+#If you prefer, you can have a look at the total experiment size (N) needed for the whole experiment, instead of group sizes (n). In addition, you can also generate static plots rather than interactive plots and html documents
 d=powerEstimator(dataset = dataset_long,refgroup = 'controls', GroupSizeBasis = F, interactivePlot = F, seedlevel = 2)
 
 #Note: you may wonder what the Cumulative is, It mainly represents the difference in distribution of our data (model) versus a null model. Hence, it can also show on average how much sample size would be needed to indicate a difference in our settings against a null hypothesis. Nonetheless, group wise levels are more definitive towards drawing implications based on individual contrasts between the respective groups against the controls. Therefore, the variable cumulative would give you some holistic view, while each individual group requiring specific sample size thresholds to represent significant differences.
